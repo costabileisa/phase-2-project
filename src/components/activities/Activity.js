e7cc31d86aeea1747761df4ecb88d838972b76d9
Isabella Costabile <costabileisa@gmail.com>
2022-12-23 20:36:58 -0500

    package additions

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Activity({ act }) {
     const { activity, energy } = act;
+     const [upvote, setUpvote] = useState(0)
 
+    function handleClick() {
+        setUpvote(current => current + 1)
+    }
+
+    // make a button that holds upvotes
+    // state for upvote
+    // create button for each activity
+    // on button click update state
     return (
         <tr className="activity-row">
-            <td className="activity-data">{activity}</td>
+            <td className="activity-data">
+                {activity}
+                </td>
             <td className="activity-data">{energy}</td>
         </tr>
     )
